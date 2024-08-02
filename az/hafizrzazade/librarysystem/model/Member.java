db2e40c0b2afffc899b3c525ac1b17e8fbf08514
Exstaa <exstagithub@gmail.com>
2024-08-02 21:26:56 +0400

    Member model is ready.

---
+++ az/hafizrzazade/librarysystem/model/Member.java
@@ -0,0 +1,24 @@
+package az.hafizrzazade.librarysystem.model;
+
+public class Member {
+	private String id;
+	private String username;
+	private String password;
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+}
