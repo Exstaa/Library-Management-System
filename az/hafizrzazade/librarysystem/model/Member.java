ce8b1d6edaf1ca7d830b4212588568bc342a0457
Hafiz <108272533+Exstaa@users.noreply.github.com>
2024-08-05 22:18:44 +0400

    Update Member.java

--- az/hafizrzazade/librarysystem/model/Member.java
+++ az/hafizrzazade/librarysystem/model/Member.java
@@ -5,6 +5,22 @@ public class Member {
 	private String username;
 	private String password;
 
+	public Member() {
+		
+	}
+	
+	public Member(String username, String password) {
+		this.username = username;
+		this.password = password;
+	}
+	
+	public Member(String id, String username, String password) {
+		super();
+		this.id = id;
+		this.username = username;
+		this.password = password;
+	}
+
 	public String getUsername() {
 		return username;
 	}
